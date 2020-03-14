5a0cb41726eab179af520e1589455a14e0fe6fb0
HankinSimida <hankin_06@outlook.com>
2020-03-15 01:42:02 +0800

    create cluster of eureka

--- cloud-consumer-order80/src/main/java/cn/hankin/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/cn/hankin/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package cn.hankin.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hankin
  * @date 2020/3/14 23:01
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cloud-eureka-service7001/src/main/java/cn/hankin/springcloud/EurekaMain7001.java
+++ cloud-eureka-service7001/src/main/java/cn/hankin/springcloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package cn.hankin.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author hankin
- * @date 2020/3/8 21:50
+ * @date 2020/3/15 0:24
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
