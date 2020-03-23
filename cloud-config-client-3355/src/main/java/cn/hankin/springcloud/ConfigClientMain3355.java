053f09a1756d27e4dbba085dde8fb77a55909b77
HankinSimida <hankin_06@outlook.com>
2020-03-24 01:17:43 +0800

    spring cloud config git

---
+++ cloud-config-client-3355/src/main/java/cn/hankin/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package cn.hankin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author hankin
+ * @date 2020/3/24 0:44
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/cn/hankin/springcloud/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package cn.hankin.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hankin
+ * @date 2020/3/24 0:48
+ */
+@RestController
+@RefreshScope
+public class ConfigController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
