fca940ecc7a7ee4586be7295144f04366a6e078e
HankinSimida <hankin_06@outlook.com>
2020-03-29 03:26:09 +0800

    spring cloud alibaba nacos

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/hankin/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package cn.hankin.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hankin
+ * @date 2020/3/28 22:33
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/hankin/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package cn.hankin.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hankin
+ * @date 2020/3/28 22:34
+ */
+@RestController
+@RefreshScope //支持动态刷新
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
