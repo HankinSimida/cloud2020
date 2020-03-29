5557c7f6966cfae8ea97ef6bc8fc0ca04e777ab2
HankinSimida <hankin_06@outlook.com>
2020-03-29 16:36:19 +0800

    spring cloud alibaba sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/cn/hankin/springcloud/alibaba/MainApp8401.java
@@ -0,0 +1,17 @@
+package cn.hankin.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hankin
+ * @date 2020/3/29 15:23
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class,args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/cn/hankin/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,42 @@
+package cn.hankin.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author hankin
+ * @date 2020/3/29 15:24
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA(){
+        try {
+            TimeUnit.MILLISECONDS.sleep(800);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return "-----testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        return "-----testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        log.info("testD");
+        return "---testD";
+    }
+}
